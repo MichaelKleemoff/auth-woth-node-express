017f7047184f50d998e79958e36f7074576c6ffb
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-20 10:37:11 -0500

    Update welcome message in home route in 'app.js'.

--- auth-express-backend/app.js
+++ auth-express-backend/app.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const usersController = require("./controllers/usersController");
+const usersController = require('./controllers/usersController');
 app.use(cors());
 app.use(express.json());
-app.use("/users", usersController);
+app.use('/users', usersController);
 
-app.get("/", (req, res) => {
-  res.send("welcome to Authentication with Node and Express");
-}
-);
+app.get('/', (req, res) => {
+	res.send('Welcome to Authentication with Node and Express');
+});
 
-app.get("*", (req, res) => {
-  res.status(404).json({ success: false, data: { error: "page not found" } });
+app.get('*', (req, res) => {
+	res.status(404).json({ success: false, data: { error: 'page not found' } });
 });
 module.exports = app;
