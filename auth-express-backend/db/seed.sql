192fe46130bf284ce882bb6e96ed9a65b9803be7
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-20 10:24:40 -0500

    Update 'schema.sql', 'seed.sql'.

--- auth-express-backend/db/seed.sql
+++ auth-express-backend/db/seed.sql
@@ -1,6 +1,6 @@
 \c auth_dev;
 
-INSERT INTO users (firstname, lastname, email, password)
+INSERT INTO users (username, email, password_hash)
 VALUES
-('Gigi', 'Scarborough', 'gigi@me.com', '$2a$10$o5/9VUbgLhN/IEJQ7jRJOO4FZDrL5yENb6GtFrWoAX7r92z7OXeem'),
-('Tim', 'McKiernan', 'tim@me.com', '$2a$10$vJHRxxeVwXo1fAoc.0aRY.kVIWghxlZbkyIwrKoxX1UNYkIGUjQqC');
+('gigi_scarborough', 'gigi@me.com', '$2a$10$o5/9VUbgLhN/IEJQ7jRJOO4FZDrL5yENb6GtFrWoAX7r92z7OXeem'),
+('tim_mckiernan', 'tim@me.com', '$2a$10$vJHRxxeVwXo1fAoc.0aRY.kVIWghxlZbkyIwrKoxX1UNYkIGUjQqC');
