9a6c5868b1b2817dc18f5c5bbe7d651a7cd5fbce
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-20 10:04:04 -0500

    Add 'private_key' to 'dbConfig.js'. Install 'cookie-parser', 'express-validator', 'passport-jwt' packages.

--- auth-express-backend/db/dbConfig.js
+++ auth-express-backend/db/dbConfig.js
@@ -1,14 +1,15 @@
-const pgp = require("pg-promise")();
+const pgp = require('pg-promise')();
 
 require('dotenv').config();
 
 const cn = {
-    host: process.env.PG_HOST, 
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER
+	host: process.env.PG_HOST,
+	port: process.env.PG_PORT,
+	database: process.env.PG_DATABASE,
+	user: process.env.PG_USER,
+	private_key: process.env.PRIVATE_KEY,
 };
 
 const db = pgp(cn);
 
-module.exports = db;    
+module.exports = db;
